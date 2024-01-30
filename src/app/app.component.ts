b8ba2cf29bc2611e991f8d501ccfc50ba29ec08b
Willian <correo.willianrueda@gmail.com>
2024-01-30 11:06:09 -0500

    Implementacion de ng template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-template';
+  visible: boolean = true;
+
+  mostrar() {
+    this.visible = !this.visible;
+}
 }
